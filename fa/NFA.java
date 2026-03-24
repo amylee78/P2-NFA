94e19a229a841b3918785195b26233f73bf6bfa0
amylee78 <142922298+amylee78@users.noreply.github.com>
2026-03-24 11:11:38 -0600

    added needed files.

---
+++ fa/NFA.java
@@ -0,0 +1,61 @@
+package fa;
+
+import java.util.Set;
+
+public class NFA  implements FAInterface {
+
+    @Override
+    public boolean addState(String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addState'");
+    }
+
+    @Override
+    public boolean setFinal(String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setFinal'");
+    }
+
+    @Override
+    public boolean setStart(String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setStart'");
+    }
+
+    @Override
+    public void addSigma(char symbol) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addSigma'");
+    }
+
+    @Override
+    public boolean accepts(String s) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'accepts'");
+    }
+
+    @Override
+    public Set<Character> getSigma() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getSigma'");
+    }
+
+    @Override
+    public State getState(String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getState'");
+    }
+
+    @Override
+    public boolean isFinal(String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isFinal'");
+    }
+
+    @Override
+    public boolean isStart(String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isStart'");
+    }
+    
+}
